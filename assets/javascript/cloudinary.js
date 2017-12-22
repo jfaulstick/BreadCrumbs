75ebe711141d2364fae676b44c6ccef85e55cff6
Joe Faulstick <jfaulstick@gmail.com>
2017-12-21 20:31:11 -0800

    Got Cloudinary upload working. Please change cloud_name and upload_preset to your own values when testing in your own branches

--- assets/javascript/cloudinary.js
+++ assets/javascript/cloudinary.js
@@ -1,15 +1,13 @@
 // Configure cloudinary
-$.cloudinary.config({ cloud_name: 'djzxhcr1g', upload_preset: "dzrlj6sb", secure: true});
+// var cl = new cloudinary.Cloudinary({cloud_name: "djzxhcr1g", upload_preset: "dzrlj6sb"});
 
-$(document).ready(function() {
-
-	if ($.fn.cloudinary_fileupload !== undefined) {
-		$("input.cloudinary-fileupload[type=file]").unsigned_cloudinary_fileupload("dzrlj6sb", {
-			cloud_name: 'djzxhcr1g',
-			tags: 'browser_uploads' }
-		});
-	}
+$(document).ready(function () {
+// $(“#cloudinary-input”).attr(“data-form-data”, jsonData);
+console.log(Math.floor(Date.now() / 1000));
 });
 
-$(".cloudinary_fileupload").append($.cloudinary.unsigned_upload_tag("dzrlj6sb", {
-	cloud_name: 'djzxhcr1g' }));
+$(document).ready(function() {
+  if($.fn.cloudinary_fileupload !== undefined) {
+    $("input.cloudinary-fileupload[type=file]").cloudinary_fileupload();
+  }
+});
